1d1d53d8de30f74e749b6474f7229dca5a5322a3
linyows <linyows@gmail.com>
2015-12-30 14:29:09 +0900

    update cli flags

--- cli.go
+++ cli.go
@@ -27,14 +27,22 @@ func (cli *CLI) Run(args []string) int {
 	var ops Ops
 	flags.StringVar(&ops.Config, "config", "", "Pox config path")
 	flags.StringVar(&ops.Config, "c", "", "Pox config path(Short)")
+
 	flags.StringVar(&ops.Root, "root", "/var/www/pox", "Pox response document root")
 	flags.StringVar(&ops.Root, "r", "/var/www/pox", "Pox response document root(Short)")
-	flags.BoolVar(&ops.Verbose, "verbose", false, "Print verbose log")
-	flags.BoolVar(&ops.Verbose, "v", false, "Print verbose log(Short)")
+
+	flags.StringVar(&ops.Addr, "addr", "localhost:8080", "Server address with port")
+	flags.StringVar(&ops.Addr, "a", "localhost:8080", "Server address with port(Short)")
+
+	flags.StringVar(&ops.Loglevel, "loglevel", "info", "Log level")
+	flags.StringVar(&ops.Loglevel, "l", "info", "Log level(Short)")
+
 	flags.IntVar(&ops.Delay, "delay", 1, "Delay seconds for response")
 	flags.IntVar(&ops.Delay, "d", 1, "Delay seconds for response(Short)")
-	flags.StringVar(&ops.Type, "type", "rest", "Api type")
-	flags.StringVar(&ops.Type, "t", "rest", "Api type(Short)")
+
+	flags.StringVar(&ops.Type, "type", "REST", "Api type")
+	flags.StringVar(&ops.Type, "t", "REST", "Api type(Short)")
+
 	flags.BoolVar(&ops.Version, "version", false, "Print version information and quit.")
 
 	if err := flags.Parse(args[1:]); err != nil {
