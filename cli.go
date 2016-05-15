05b051d404f54167d066a685f873959cbb7cf64e
linyows <linyows@gmail.com>
2016-05-16 02:37:26 +0900

    fix for cli options

--- cli.go
+++ cli.go
@@ -26,29 +26,49 @@ func (cli *CLI) Run(args []string) int {
 	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
 	flags.SetOutput(cli.errStream)
 
+	flags.Usage = func() {
+		fmt.Fprintf(os.Stderr, "\nUsage: %s [options]\n\nOptions:\n", Name)
+		flags.VisitAll(func(f *flag.Flag) {
+			if len(f.Name) == 1 {
+				s := fmt.Sprintf("  -%s", f.Name)
+				fmt.Fprint(os.Stderr, s, ",")
+			} else {
+				s := fmt.Sprintf(" --%s", f.Name)
+				_, usage := flag.UnquoteUsage(f)
+				num := 12 - len(f.Name)
+				s += strings.Repeat(" ", num) + usage
+				if !(f.DefValue == "" || f.DefValue == "false" || f.DefValue == "0") {
+					s += fmt.Sprintf(" (default %v)", f.DefValue)
+				}
+				fmt.Fprint(os.Stderr, s, "\n")
+			}
+		})
+	}
+
 	conf := os.Getenv(strings.ToUpper(Name) + "_CONF")
 	c := DefaultConfig()
 
 	var ops Ops
-	flags.StringVar(&ops.Config, "config", conf, "Pox config path")
-	flags.StringVar(&ops.Config, "c", conf, "Pox config path(Short)")
+	flags.StringVar(&ops.Config, "config", conf, "config path")
+	flags.StringVar(&ops.Config, "c", conf, "")
 
-	flags.StringVar(&ops.Root, "root", c.Root, "Pox response document root")
-	flags.StringVar(&ops.Root, "r", c.Root, "Pox response document root(Short)")
+	flags.StringVar(&ops.Root, "root", c.Root, "document root")
+	flags.StringVar(&ops.Root, "r", c.Root, "")
 
-	flags.StringVar(&ops.Addr, "addr", c.Addr, "Server address with port")
-	flags.StringVar(&ops.Addr, "a", c.Addr, "Server address with port(Short)")
+	flags.StringVar(&ops.Addr, "addr", c.Addr, "address with port")
+	flags.StringVar(&ops.Addr, "a", c.Addr, "")
 
-	flags.StringVar(&ops.LogLevel, "log-level", c.LogLevel, "Log level")
-	flags.StringVar(&ops.LogLevel, "l", c.LogLevel, "Log level(Short)")
+	flags.StringVar(&ops.LogLevel, "log-level", c.LogLevel, "log level")
+	flags.StringVar(&ops.LogLevel, "l", c.LogLevel, "")
 
-	flags.IntVar(&ops.Delay, "delay", c.Delay, "Delay seconds for response")
-	flags.IntVar(&ops.Delay, "d", c.Delay, "Delay seconds for response(Short)")
+	flags.IntVar(&ops.Delay, "delay", c.Delay, "delay seconds for response")
+	flags.IntVar(&ops.Delay, "d", c.Delay, "")
 
-	flags.StringVar(&ops.Protocol, "protocol", c.Protocol, "Api Protocol")
-	flags.StringVar(&ops.Protocol, "p", c.Protocol, "Api Protocol(Short)")
+	flags.StringVar(&ops.Protocol, "protocol", c.Protocol, "api protocol")
+	flags.StringVar(&ops.Protocol, "p", c.Protocol, "")
 
-	flags.BoolVar(&ops.Version, "version", false, "Print version information and quit.")
+	flags.BoolVar(&ops.Version, "version", false, "print the version and exit")
+	flags.BoolVar(&ops.Version, "v", false, "")
 
 	if err := flags.Parse(args[1:]); err != nil {
 		return ExitCodeError
